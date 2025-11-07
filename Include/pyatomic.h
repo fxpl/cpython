20f975085ac12030c0f6261bf5d0d049244a9fe3
Matthew Parkinson <mattpark@microsoft.com>
2025-11-07 21:45:34 +0000

    Make incref a function for immutable

--- Include/pyatomic.h
+++ Include/pyatomic.h
@@ -4,12 +4,11 @@
 extern "C" {
 #endif
 
-// TODO(Immutable): Hack as some refcount.h build needs this, and it wasn't on. Investigate later.
-//#ifndef Py_LIMITED_API
+#ifndef Py_LIMITED_API
 #  define Py_CPYTHON_ATOMIC_H
 #  include "cpython/pyatomic.h"
 #  undef Py_CPYTHON_ATOMIC_H
-//#endif
+#endif
 
 #ifdef __cplusplus
 }
