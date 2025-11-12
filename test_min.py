dae46bff30b87d277423e5bbccc36b5c495c17a7
Matthew Parkinson <mattpark@microsoft.com>
2025-11-12 20:16:44 +0000

    min failure, need to turn this into a test

--- test_min.py
+++ test_min.py
@@ -1,37 +1,10 @@
-from immutable import freeze, NotFreezable, isfrozen
-from gc import collect
-from sys import getrefcount
-class C:
-    def __init__(self):
-        self.x = 0
+# import unittest
+from immutable import freeze
 
-    def set(self, x):
-        d = self.__dict__
-        d['x'] = x
+class QName:
+    def foo(self):
+        return QName
+    def bar(self):
+        return QName
 
-def test_dict_mutation():
-    print("********************Start obj = C()   ***************getrefcount(C):", getrefcount(C))
-    obj = C()
-    print("********************End obj = C()   ***************getrefcount(C):", getrefcount(C))
-    print("********************Start obj.set(1)  ***************getrefcount(C):", getrefcount(C))
-    obj.set(1)
-    print("********************End obj.set(1)  ***************getrefcount(C):", getrefcount(C))
-    print("********************Start freeze(obj)  ***************getrefcount(C):", getrefcount(C))
-    freeze(obj)
-    print("********************End freeze(obj)  ***************getrefcount(C):", getrefcount(C))
-
-if __name__ == '__main__':
-    print("*******************************************************************Freeze C")
-    print("***********************************getrefcount(C):", getrefcount(C))
-    freeze(C)
-    print("*******************************************************************Testing dict mutation")
-    print("***********************************getrefcount(C):", getrefcount(C))
-    test_dict_mutation()
-    print("*******************************************************************Testing dict mutation 2")
-    print("***********************************getrefcount(C):", getrefcount(C))
-    test_dict_mutation()
-    print("*******************************************************************Collecting")
-    print("***********************************getrefcount(C):", getrefcount(C))
-    collect()
-    print("*******************************************************************Done")
-    print("***********************************getrefcount(C):", getrefcount(C))
+freeze(QName)
