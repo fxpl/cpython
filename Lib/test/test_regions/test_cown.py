6091f9c232a4445dde96925c1d28716e292f277e
xFrednet <xFrednet@gmail.com>
2025-11-19 11:55:07 +0100

    Test cown.value field

--- Lib/test/test_regions/test_cown.py
+++ Lib/test/test_regions/test_cown.py
@@ -42,7 +42,53 @@ class TestBasicCownObject(unittest.TestCase):
         r.x = x
         self.assertTrue(r.owns(x))
 
-        # Local arguments are forbidden
+        # Owned arguments are forbidden
         with self.assertRaises(RuntimeError) as e:
             Cown(x)
+
+class TestCownValueField(unittest.TestCase):
+    def test_cown_valid_value_fields(self):
+        # No argument, is a good argument
+        c = Cown()
+        self.assertIsNone(c.value)
+
+        # Immortal builtin object
+        c.value = None
+        self.assertIsNone(c.value)
+
+        # Frozen object
+        x = {}
+        freeze(x)
+        c.value = x
+        self.assertEqual(c.value, x)
+
+        # Cowns are allowed
+        c1 = Cown(c)
+        c.value = c1
+        self.assertEqual(c.value, c1)
+
+        # Regions are allowed
+        r = Region(name="dummy")
+        c.value = r
+        self.assertEqual(c.value, r)
+
+    def test_cown_value_error_for_local(self):
+        x = {}
+        self.assertTrue(is_local(x))
+        c = Cown()
+
+        # Local values are forbidden
+        with self.assertRaises(RuntimeError) as e:
+            c.value = x
+
+    def test_cown_construction_error_for_owned(self):
+        r = Region()
+        x = {}
+        r.x = x
+        self.assertTrue(r.owns(x))
+
+        c = Cown()
+        # Owned values are forbidden
+        with self.assertRaises(RuntimeError) as e:
+            c.value = x
 
