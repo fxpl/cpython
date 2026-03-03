bf41be4cccf3d9614f94771ee28893deef2e50d5
Matthew Parkinson <mattpark@microsoft.com>
2026-03-03 10:27:57 +0000

    Regen-all

--- Modules/clinic/_immutablemodule.c.h
+++ Modules/clinic/_immutablemodule.c.h
@@ -2,30 +2,30 @@
 preserve
 [clinic start generated code]*/
 
-PyDoc_STRVAR(immutable_register_freezable__doc__,
+PyDoc_STRVAR(_immutable_register_freezable__doc__,
 "register_freezable($module, obj, /)\n"
 "--\n"
 "\n"
 "Register a type as freezable.");
 
-#define IMMUTABLE_REGISTER_FREEZABLE_METHODDEF    \
-    {"register_freezable", (PyCFunction)immutable_register_freezable, METH_O, immutable_register_freezable__doc__},
+#define _IMMUTABLE_REGISTER_FREEZABLE_METHODDEF    \
+    {"register_freezable", (PyCFunction)_immutable_register_freezable, METH_O, _immutable_register_freezable__doc__},
 
-PyDoc_STRVAR(immutable_freeze__doc__,
+PyDoc_STRVAR(_immutable_freeze__doc__,
 "freeze($module, obj, /)\n"
 "--\n"
 "\n"
 "Freeze an object and its graph.");
 
-#define IMMUTABLE_FREEZE_METHODDEF    \
-    {"freeze", (PyCFunction)immutable_freeze, METH_O, immutable_freeze__doc__},
+#define _IMMUTABLE_FREEZE_METHODDEF    \
+    {"freeze", (PyCFunction)_immutable_freeze, METH_O, _immutable_freeze__doc__},
 
-PyDoc_STRVAR(immutable_isfrozen__doc__,
+PyDoc_STRVAR(_immutable_isfrozen__doc__,
 "isfrozen($module, obj, /)\n"
 "--\n"
 "\n"
 "Check if an object is frozen.");
 
-#define IMMUTABLE_ISFROZEN_METHODDEF    \
-    {"isfrozen", (PyCFunction)immutable_isfrozen, METH_O, immutable_isfrozen__doc__},
-/*[clinic end generated code: output=580876fead975241 input=a9049054013a1b77]*/
+#define _IMMUTABLE_ISFROZEN_METHODDEF    \
+    {"isfrozen", (PyCFunction)_immutable_isfrozen, METH_O, _immutable_isfrozen__doc__},
+/*[clinic end generated code: output=95f703fa7ad82910 input=a9049054013a1b77]*/
